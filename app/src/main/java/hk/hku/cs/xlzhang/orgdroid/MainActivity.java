c9deba92205fe8c5f5c756d6fba5a3e6794e590c
Yating Yue <ytyue2305@gmail.com>
2015-11-18 17:22:02 +0800

    Modify Tab.

--- app/src/main/java/hk/hku/cs/xlzhang/orgdroid/MainActivity.java
+++ app/src/main/java/hk/hku/cs/xlzhang/orgdroid/MainActivity.java
@@ -1,5 +1,6 @@
 package hk.hku.cs.xlzhang.orgdroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TabHost;
+import android.widget.TabHost.TabContentFactory;
+import android.widget.TabHost.TabSpec;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +24,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // create the TabHost that will contain the Tabs
+        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+        tabHost.setup();
+
+        final TabWidget tabWidget = tabHost.getTabWidget();
+        final FrameLayout tabContent = tabHost.getTabContentView();
+
+        // Get the original tab textviews and remove them from the viewgroup.
+        TextView[] originalTextViews = new TextView[tabWidget.getTabCount()];
+        for (int index = 0; index < tabWidget.getTabCount(); index++) {
+            originalTextViews[index] = (TextView) tabWidget.getChildTabViewAt(index);
+        }
+        tabWidget.removeAllViews();
+
+        // Ensure that all tab content childs are not visible at startup.
+        for (int index = 0; index < tabContent.getChildCount(); index++) {
+            tabContent.getChildAt(index).setVisibility(View.GONE);
+        }
+
+        // Create the tabspec based on the textview childs in the xml file.
+        // Or create simple tabspec instances in any other way...
+        for (int index = 0; index < originalTextViews.length; index++) {
+            final TextView tabWidgetTextView = originalTextViews[index];
+            final View tabContentView = tabContent.getChildAt(index);
+            TabSpec tabSpec = tabHost.newTabSpec((String) tabWidgetTextView.getTag());
+            tabSpec.setContent(new TabContentFactory() {
+                @Override
+                public View createTabContent(String tag) {
+                    return tabContentView;
+                }
+            });
+            if (tabWidgetTextView.getBackground() == null) {
+                tabSpec.setIndicator(tabWidgetTextView.getText());
+            } else {
+                tabSpec.setIndicator(tabWidgetTextView.getText(), tabWidgetTextView.getBackground());
+            }
+            tabHost.addTab(tabSpec);
+        }
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
